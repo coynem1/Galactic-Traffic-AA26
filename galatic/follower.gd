d8e72d50aff2214a289845fd07c520b900d673fb
ciaranDesktop <ciaran.coyne101@gmail.com>
2026-05-02 21:29:43 +0100

    fixed static ships

--- galatic/follower.gd
+++ galatic/follower.gd
@@ -2,17 +2,30 @@ extends "res://Scripts/boid.gd"
 
 @onready var spaceship: Node3D = $spaceship
 
+var _is_dying: bool = false
 var leader: bool = false
 
 func _ready():
 	super._ready()
-	offsetPursueEnabled = true
+	seekEnabled = true
+	
+	if leader:
+		offsetPursueEnabled = true
+
+func _physics_process(delta: float) -> void:
+	if not leader:
+		_followerMovement()
+	
+	super._physics_process(delta)
+
+func _followerMovement():
+	if leaderBoid != null and is_instance_valid(leaderBoid):
+		seekTarget = leaderBoid.global_transform.origin + leaderBoid.transform.basis * leaderOffset
+	else:
+		# Leader is dead so wander
+		if not _is_dying:
+			seekEnabled = false
+			jitterWanderEnabled = true
 
 func set_colour(colour: Color, full: bool):
 	spaceship.set_colour(colour, full)
-
-#func _physics_process(delta: float) -> void:
-	#super._physics_process(delta)
-	### Match leader's rotation
-	##if leaderBoid != null:
-		##rotation = leaderBoid.rotation
